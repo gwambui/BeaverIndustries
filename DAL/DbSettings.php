a1b122acd10f13e41be2b0c56660ecd14efdff29
gwambui <wambui.kin@gmail.com>
2018-04-20 09:29:35 -0400

    Update DbSettings.php

--- DAL/DbSettings.php
+++ DAL/DbSettings.php
@@ -1,18 +1,15 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: bwood
- * Date: 2017-07-07
- * Time: 11:52 AM
  */
 
-define("DB_NAME", "beaveri1_beaver");
-define("DB_USER", "beaveri1_beaver");
-define("DB_PASSWORD", "*gossipCorner1");
-define("DB_URL", "beaverindustries.co.ke");
+define("DB_NAME", "secret");
+define("DB_USER", "secret");
+define("DB_PASSWORD", "secret");
+define("DB_URL", "localhost");
 define("DB_PORT", "3306");
 /*
-define("DB_NAME", "beaveri1_beaver");
+define("DB_NAME", "secret");
 define("DB_USER", "root");
 define("DB_PASSWORD", "secret");
 define("DB_URL", "localhost");
