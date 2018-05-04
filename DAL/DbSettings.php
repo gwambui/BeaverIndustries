e5f57a3b4cb8d9c3a37f73641d458b2bc709807b
gwambui <wambui.kin@gmail.com>
2018-05-03 20:10:17 -0400

    9:48 am Apr 20 2018

--- DAL/DbSettings.php
+++ DAL/DbSettings.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  */
 
-define("DB_NAME", "secret");
-define("DB_USER", "secret");
-define("DB_PASSWORD", "secret");
-define("DB_URL", "localhost");
+define("DB_NAME", "beaveri1_beaver");
+define("DB_USER", "beaveri1_beaver");
+define("DB_PASSWORD", "*gossipCorner1");
+define("DB_URL", "beaverindustries.co.ke");
 define("DB_PORT", "3306");
 /*
-define("DB_NAME", "secret");
+define("DB_NAME", "beaveri1_beaver");
 define("DB_USER", "root");
 define("DB_PASSWORD", "secret");
 define("DB_URL", "localhost");
