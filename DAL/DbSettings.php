1cb24162ce1f56231abb7ad8f3541dc0b0d0b439
Wambui Kinyanjui <wambui.kin@gmail.com>
2018-05-04 09:43:04 -0400

    Update DbSettings.php

--- DAL/DbSettings.php
+++ DAL/DbSettings.php
@@ -1,16 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- */
-
-define("DB_NAME", "secret");
-define("DB_USER", "secret");
-define("DB_PASSWORD", "secret");
-define("DB_URL", "localhost");
-define("DB_PORT", "3306");
-/*
-define("DB_NAME", "secret");
-define("DB_USER", "root");
-define("DB_PASSWORD", "secret");
-define("DB_URL", "localhost");
-define("DB_PORT", "3306");*/
